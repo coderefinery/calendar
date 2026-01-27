47e9437e1ce4efb2e6662f44a10f4fbc39378420
Richard Darst <rkd@zgib.net>
2026-01-27 22:25:50 +0200

    build: Fix build for newest git-calendar

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-git-calendar calendars/*.yaml -o out -i out/index.html -b out/body.html \
+git-calendar calendars/*.yaml out -i index.html -b body.html \
     --timezone=Europe/Helsinki \
     --timezone=Europe/Stockholm \
     --base-url=https://coderefinery.github.io/calendar/ \
